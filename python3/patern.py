ffa777f9578123abe5dcdf1005dc41798e5a1d01
Aftabkhan2055 <53826517+Aftabkhan2055@users.noreply.github.com>
2019-08-07 14:47:00 +0530

    Delete patern.py

--- python3/patern.py
+++
@@ -1,8 +0,0 @@
-for i in range (1,5):
-    for j in range( 1,i+1):
-        print("*",end=" ")
-    for k in range((8-2*i)+1):
-        print(" ",end=" ")
-    for r in range(1,i+1):
-        print ("*",end=" ")
-    print()   
