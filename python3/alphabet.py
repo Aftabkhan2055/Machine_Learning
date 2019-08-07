e966edfa06dc000f275c8fe1e6c13e61fc41434f
Aftabkhan2055 <53826517+Aftabkhan2055@users.noreply.github.com>
2019-08-07 14:37:55 +0530

    Delete alphabet.py

--- python3/alphabet.py
+++
@@ -1,5 +0,0 @@
-char=(input("enetr the  no"))
-if char=="a" or char=="e" or char=="i" or char=="o" or char=="u" or char=="A" or char=="E" or char=="I" or char=="O" or char=="U":
-    print("vowel")
-else:
-    print("other character")
