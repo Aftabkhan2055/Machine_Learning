960106c2c1e0cb6024653d02a3b93aef4f1c9cf8
Aftabkhan2055 <53826517+Aftabkhan2055@users.noreply.github.com>
2019-08-07 14:46:06 +0530

    Delete largest.py

--- python3/largest.py
+++
@@ -1,9 +0,0 @@
-a=int(input("enter the no"))
-b=int(input("enter the no"))
-c=int(input("enter the no"))
-if a>=b and a>=c:
-    print ("largest no a")
-if b>=a and b>=c:
-    print ("largest no b")
-if c>=a and c>=b:
-    print ("largest no c")
